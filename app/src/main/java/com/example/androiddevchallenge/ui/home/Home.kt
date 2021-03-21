b630a7b47d2ad361f4abf3cd531e9d28c4de8798
Dartek12 <b.biernacki97@gmail.com>
2021-03-22 00:08:47 +0100

    Added weather canvas.

--- app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
@@ -4,21 +4,17 @@ import android.util.Log
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -26,21 +22,21 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastMap
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.model.Forecast
@@ -115,7 +111,11 @@ fun Summary(modifier: Modifier = Modifier, state: WeatherState, place: String) {
             backgroundColor = color.value
         )
 
-        WeatherCanvas()
+        WeatherCanvas(
+            modifier = Modifier
+                .requiredHeight(400.dp)
+                .fillMaxWidth()
+        )
     }
 }
 
@@ -129,6 +129,53 @@ fun Heading(modifier: Modifier = Modifier, place: String, backgroundColor: Color
 }
 
 @Composable
-fun WeatherCanvas() {
-    Box(Modifier.height(400.dp))
+fun WeatherCanvas(modifier: Modifier = Modifier) {
+
+    SubcomposeLayout(modifier) { constraints ->
+        val layoutWidth = constraints.maxWidth
+        val layoutHeight = constraints.maxHeight
+
+        Log.d("Wompose", "$layoutWidth $layoutHeight")
+        val looseConstraints = constraints.copy(minWidth = 0, minHeight = 0)
+
+        layout(layoutWidth, layoutHeight) {
+            val halfWidth = layoutWidth / 2.0f
+            val thirdOfHeight = layoutHeight / 3.0f
+
+            val sun = subcompose("sun") {
+                Image(
+                    painter = painterResource(R.drawable.ic_sun),
+                    contentDescription = "sun",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .requiredWidth(96.dp)
+                        .aspectRatio(1.0f)
+                )
+            }.fastMap { it.measure(looseConstraints) }.first()
+
+            val clouds = subcompose("cloud-left") {
+                for (i in 0 until 2) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_cloud),
+                        contentDescription = "cloud",
+                        modifier = Modifier
+                            .requiredWidth(144.dp)
+                            .aspectRatio(1.3333f)
+                    )
+                }
+            }.fastMap { it.measure(looseConstraints) }
+
+            sun.place(
+                x = (halfWidth - sun.width / 2.0f).toInt(),
+                y = (thirdOfHeight - sun.height / 2.0f).toInt(),
+                zIndex = 0.0f
+            )
+            clouds[0].place(x = 20, y = 20, zIndex = 1.0f)
+            clouds[1].place(
+                x = (halfWidth - clouds[1].width / 6.0f).toInt(),
+                y = (thirdOfHeight - clouds[1].height / 3.0f).toInt(),
+                zIndex = 2.0f
+            )
+        }
+    }
 }
